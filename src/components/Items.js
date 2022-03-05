6c8bb338b5fadee197d5737b949171b83647873c
Eduardo Afonso <oeduardoafonso@gmail.com>
2022-03-05 00:54:00 -0300

    Corrigindo logica e integraçao do componente de Items
    Co-authored-by: Rafael Cleydson <rafael.cleydson@gmail.com>

--- src/components/Items.js
+++ src/components/Items.js
@@ -3,29 +3,44 @@ import Box from '@mui/material/Box';
 import Grid from "@mui/material/Grid";
 import CustomizedCard from "../components/Card";
 
-const items = require("../data/items.json");
 
-function cardItems() {
-    return items.map(item => (
-        <Grid
-            item
-            xs={6}
-        >
-            <CustomizedCard item={item} isSelected={true} />
-        </Grid >
-    ))
-}
 
 export default function Items(props) {
+    const items = require("../data/items.json");
 
-    return <Box {...props} sx={{display:"flex", justifyContent:"space-around"}}>
+    const handleClick = (e) => {
+        const index = props.selectedItems.indexOf(e.target.alt)
+
+        if (index === -1 && !props.isFull)
+            props.callback([...props.selectedItems, e.target.alt])
+        else if(index !== -1){
+            console.log(props.selectedItems[index])
+            let oldSelectedItems = [...props.selectedItems]
+            oldSelectedItems.splice(index, 1)
+            props.callback(oldSelectedItems)
+            props.callbackWeight(false)
+        }
+    }
+
+    const cardItems = () => {
+        return items.map(item => (
+            <Grid
+                item
+                xs={4}
+            >
+                <CustomizedCard item={item} onClick={handleClick}  isSelected={props.selectedItems.indexOf(item.name) !== -1 ? true : false} />
+            </Grid >
+        ))
+    }
+
+    return <Box {...props} sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
         <Grid
             container
-            spacing={{ xs: 24}}
+            spacing={{ xs: 24 }}
             alignItems="center"
-            justify="center"
+            justifyContent="center"
             display="flex"
-            sx={{ marginLeft: 0 }}
+            sx={{ marginLeft: 0, p: 1, marginBottom: 25 }}
         >
             {cardItems()}
 
